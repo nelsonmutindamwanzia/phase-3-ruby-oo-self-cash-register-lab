edcdc8d343fdb0b9db35fedbcb21d676ef3602ab
nelsonmutindamwanzia <nmutinda02@gmail.com>
2022-10-20 16:03:56 +0300

    initialize variables, add method base code

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,23 @@
+class CashRegister
 
+    #items, discount, total and last_transaction methods need to read and write
+    attr_accessor :items, :discount, :total, :last_transaction
+
+    def initialize(discount = 0)
+        @total = 0
+        @discount = discount
+        @items = []
+    end
+
+    def add_item
+
+    end
+
+    def apply_discount
+
+    end
+
+    def void_last_transaction
+
+    end
+end
